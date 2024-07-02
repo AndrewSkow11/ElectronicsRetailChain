720bbce5f1d28945710dca267f32b7d7dea3736c
Andrew Skow <skovorodnikov11@gmail.com>
2024-07-02 18:28:04 +0300

    модели - ok

--- chain/admin.py
+++ chain/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from chain.models import Contact
+from chain.models import Contact, LinkOfChain
 
 
 admin.site.register(Contact)
+admin.site.register(LinkOfChain)
--- chain/models.py
+++ chain/models.py
@@ -17,16 +17,18 @@ from django.db import models
 # а если розничная сеть относится напрямую к заводу,
 # минуя остальные звенья, ее уровень — 1.
 #
-# 2. Каждое звено сети должно обладать следующими элементами:
-# - Название.
+
+
+# иерархия завода - 0
+# розничная сеть - 1 или 2
+# ИП - 1 или 2
+
 
 # - Продукты:
 #   - название,
 #   - модель,
 #   - дата выхода продукта на рынок.
-# - Поставщик (предыдущий по иерархии объект сети).
-# - Задолженность перед поставщиком в денежном выражении с точностью до копеек.
-# - Время создания (заполняется автоматически при создании).
+
 
 class Contact(models.Model):
     """Вспомогательный элемент модели звена сети"""
@@ -64,9 +66,43 @@ class Contact(models.Model):
         verbose_name_plural = "контакты"
 
 
-class Link(models.Model):
+class LinkOfChain(models.Model):
     """Модель звена сети"""
     name = models.CharField(max_length=255, verbose_name='название звена')
+    contact = models.OneToOneField(
+        Contact,
+        on_delete=models.CASCADE,
+        related_name='contact',
+        verbose_name='контакты звена'
+    )
+    supplier = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='поставщик'
+    )
+    debt = models.DecimalField(
+        max_digits=16,
+        decimal_places=2,
+        verbose_name='задолженность'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='время создания'
+    )
+
+    # - Поставщик (предыдущий по иерархии объект сети).
+    def get_hierracly_level(self):
+        """Метод определяет уровень иерархии текущего звена,
+         рекурсивно поднимаясь по цепочке поставщиков"""
+
+        def get_level(link, level=0):
+            if link.supplier:
+                return get_level(link.supplier, level + 1)
+            return level
+
+        return get_level(self)
 
     def __str__(self):
         return f'{self.name}'
@@ -74,3 +110,35 @@ class Link(models.Model):
     class Meta:
         verbose_name = "звено сети"
         verbose_name_plural = "звенья сети"
+
+class Product(models.Model):
+    # - Продукты:
+    #   - название,
+    #   - модель,
+    #   - дата выхода продукта на рынок.
+
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='название продукта'
+    )
+    model = models.CharField(
+        max_length=256,
+        verbose_name='модель продукта'
+    )
+    market_date = models.DateField(
+        verbose_name='дата выхода на рынок'
+    )
+    link_of_chain = models.ForeignKey(
+        LinkOfChain,
+        on_delete=models.CASCADE,
+        related_name='product',
+        verbose_name='звено сети'
+    )
+
+    def __str__(self):
+        return f'{self.name} - {self.model} - {self.market_date}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
